566c45545a3779d25c5dc3666c879f448a8a43e0
Tyler Carr <tcarr@harriscomputer.com>
2023-10-27 10:02:36 -0400

    Got image downloading (after accidentally locking my cameras)

--- yolo_harness.py
+++ yolo_harness.py
@@ -8,23 +8,36 @@
 #    Ignore if the same count of objects has been in the past X many frames (camera specific lookback memory)
 #  Email alerts for specific classes of objects
 
+import requests
+from requests.auth import HTTPDigestAuth
+import tempfile
+from PIL import Image
+from decouple import config
+import io
+
 class yolo_harness:
     configDict = {
         "cameras": {
-            "backyard": {"blindspots": [],
+            # "backyard": {"blindspots": [],
+            #              "objects_of_interest": ["person","bicycle","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe"],
+            #              "url": "http://192.168.254.11/ISAPI/Streaming/Channels/101/picture",
+            #              "user": config('CAM_USER'),
+            #              "password": config('CAM_PASSWORD')
+            #             },
+            "sideyard": {"blindspots": [],
                          "objects_of_interest": ["person","bicycle","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe"],
-                         "url": "",
-                         "user": "",
-                         "password": ""
+                         "url": "http://192.168.254.5/ISAPI/Streaming/Channels/101/picture",
+                         "user": config('CAM_USER'),
+                         "password": config('CAM_PASSWORD')
                         },
-            "driveway": {"blindspots": [(0.0,0.2),(1.0,0.2)],
-                         "objects_of_interest": ["car","motorcycle","bus","train","truck","person","bicycle","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe"],
-                         "url": "",
-                         "user": "",
-                         "password": ""
-                        }
+            # "driveway": {"blindspots": [(0.0,0.2),(1.0,0.2)],
+            #              "objects_of_interest": ["car","motorcycle","bus","train","truck","person","bicycle","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe"],
+            #              "url": "http://192.168.254.2/ISAPI/Streaming/Channels/101/picture",
+            #              "user": config('CAM_USER'),
+            #              "password": config('CAM_PASSWORD')
+            #             }
         },
-        "xxcamera_sequence": ["driveway","backyard"]
+        # "camera_sequence": ["driveway","backyard"]
     }
     lastFrameDict = {
         #Store information about the objects in most recent frame (and lookback period too) for each camera name "key": (and persist/reload from disk for when program restarts)
@@ -37,18 +50,45 @@ class yolo_harness:
             for key in self.configDict["cameras"]:
                 camera_sequence.append(key)
             self.configDict["camera_sequence"] = camera_sequence
+        print(self.configDict)
         self.cameraLoop()
 
     def cameraLoop(self):
         numCameras = len(self.configDict["camera_sequence"])
         currentCamera = 0
-        while True: #infinite loop
-            currentCamera=currentCamera+1
-            if(currentCamera >= numCameras):
-                currentCamera=0
-            currentCameraName = self.configDict["camera_sequence"][currentCamera]
-            cameraConfig = self.configDict["cameras"][currentCameraName]
-            print(cameraConfig)
+        # while True: #infinite loop
+        currentCamera=currentCamera+1
+        if(currentCamera >= numCameras):
+            currentCamera=0
+        currentCameraName = self.configDict["camera_sequence"][currentCamera]
+        cameraConfig = self.configDict["cameras"][currentCameraName]
+        self.processCameraImage(currentCameraName, cameraConfig)
 
+    def processCameraImage(self, currentCameraName, cameraConfig):
+        image = self.download_image(cameraConfig["url"], cameraConfig["user"], cameraConfig["password"])
+        print(image)
+
+    def download_image(self, url, username, password):
+        buffer = tempfile.SpooledTemporaryFile(max_size=1e9)
+        i = None
+        r = None
+        if username == "" or password == "":
+            r = requests.get(url, stream=True)    
+        else:
+            r = requests.get(url, auth=HTTPDigestAuth(username, password), stream=True)
+        if r.status_code == 200:
+            downloaded = 0
+            filesize = int(r.headers['content-length'])
+            for chunk in r.iter_content(chunk_size=1024):
+                downloaded += len(chunk)
+                buffer.write(chunk)
+                # print(downloaded/filesize)
+            buffer.seek(0)
+            i = Image.open(io.BytesIO(buffer.read()))
+            # i.save(os.path.join(out_dir, 'image.jpg'), quality=85)
+        else:
+            print(r)
+        buffer.close()
+        return i
 
 yh = yolo_harness()
